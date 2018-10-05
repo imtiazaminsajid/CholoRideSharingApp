ae80ce2452bbc644fac8017c38671b6343b4853d
Imtiaz Amin Sajid <imtiazaminsajid@gmail.com>
2018-10-06 02:49:08 +0600

    Login And Registration Done

--- app/src/main/java/com/example/imtiazaminsajid/cholo_ridesharingapp/MainActivity.java
+++ app/src/main/java/com/example/imtiazaminsajid/cholo_ridesharingapp/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.imtiazaminsajid.cholo_ridesharingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button nDriver, nCustomer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nDriver = (Button) findViewById(R.id.driver);
+        nCustomer = (Button) findViewById(R.id.customer);
+
+        nDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
+
+        nCustomer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CustomerLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
+            }
+        });
     }
 }
